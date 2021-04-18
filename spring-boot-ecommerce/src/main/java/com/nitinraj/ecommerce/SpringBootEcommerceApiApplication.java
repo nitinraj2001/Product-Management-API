ea392c5abe6ce75b2ececb6b9de86dca852f026b
Nitin Raj <nitinraj2001@gmail.com>
2021-04-18 10:01:42 +0530

    it's a product management system api first commit many more to come

---
+++ spring-boot-ecommerce/src/main/java/com/nitinraj/ecommerce/SpringBootEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.nitinraj.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-ecommerce/src/test/java/com/nitinraj/ecommerce/SpringBootEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitinraj.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
